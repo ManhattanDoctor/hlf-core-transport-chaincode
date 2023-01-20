724a6b42e2383fa053dc366be83f9b6769ee20e4
Renat Gubaev <renat.gubaev@gmail.com>
2023-01-20 16:39:57 +0300

    auto commit

--- src/batch/TransportFabricStubWrapper.ts
+++ src/batch/TransportFabricStubWrapper.ts
@@ -40,7 +40,7 @@ export class TransportFabricStubWrapper extends TransportFabricStub {
             return;
         }
         let item = {};
-        console.log('Settings events', this.events);
+        console.log('Settings events-', this.events);
         this.events.forEach((events, transactionHash) => item[transactionHash] = TransformUtil.fromClassMany(events));
         this.setEvent(ObjectUtil.sortKeys(item, true));
     }
